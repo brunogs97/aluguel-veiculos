a48a4075914f0735693667d2f531cddb8c057b11
Bruno Gomes <gomes_bruno97@hotmail.com>
2023-01-27 11:34:59 -0300

    Finalizada seção executivo

--- grupo-executivo.js
+++ grupo-executivo.js
@@ -6,34 +6,35 @@ const formasPagamentos = document.querySelector('#formas-de-pagamentos');
 const botaoFinalizarReserva = document.querySelector('#botao-reserva');
 
 PagamentosComAdicionais.addEventListener('click', (e) => {
-
+    const verificarMarcacao = check.checked;
     if(check) {
         PagamentosComAdicionais.style.background = '#eef1f3';
         pagamentosSemAdicionais.style.background = 'none';
     }
     
-    const valorAVista = 102.59 - (102.59 * 0.05);
+    const valorAVista = 614.20 - (614.20 * 0.05);
 
-    let resposta = `O valor de R$ 102,59 parcelado em 12x fica: ` + '\n';
+    let resposta = `O valor de R$ 614,20 parcelado em 12x fica: ` + '\n';
     for(let i = 1; i <= 12; i++) {
-        resposta = `${resposta} ${i} x 102,59 = R$ ${(102.59 / i).toFixed(2)}\n`
+        resposta = `${resposta} ${i} x 614,20 = R$ ${(614.20 / i).toFixed(2)}\n`
     }
     formasPagamentos.innerHTML = `O pagamento à vista fica R$ ${valorAVista.toFixed(2)}` + '\n' + `${resposta}`;
     
 })
     
 pagamentosSemAdicionais.addEventListener('click', (e) => {
+    const verificarMarcacao = check.checked;
 
     if(check) {
         pagamentosSemAdicionais.style.background = '#eef1f3';
         PagamentosComAdicionais.style.background = 'none';
     }
 
-    const valorAVista = 88.30 - (88.30 * 0.05);
+    const valorAVista = 628.00 - (628.00 * 0.05);
 
-    let resposta = `O valor de R$ 88,30 parcelado em 12x fica: ` + '\n';
+    let resposta = `O valor de R$ 628,00 parcelado em 12x fica: ` + '\n';
     for(let i = 1; i <= 12; i++) {
-        resposta = `${resposta} ${i} x 88,30 = R$ ${(88.30 / i).toFixed(2)}\n`
+        resposta = `${resposta} ${i} x 628,00 = R$ ${(628.00 / i).toFixed(2)}\n`
     }
     formasPagamentos.innerHTML = `O pagamento à vista fica R$ ${valorAVista.toFixed(2)}` + '\n' + `${resposta}`;
 })
